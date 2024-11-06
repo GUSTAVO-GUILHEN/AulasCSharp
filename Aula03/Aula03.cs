e638c7579411804b46372246d5857440f8a12acb
GUSTAVO-GUILHEN <gguilhen98@gmail.com>
2024-11-05 22:18:38 -0300

    Variaveis

---
+++ Aula03/Aula03.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Aula03{
+
+    static void Main(){
+
+        byte n1=10; // varia entre 0 e 255
+        int num=-10;
+        char letra='c';
+        float valor=5.3f; 
+        string nome="Gustavo";
+
+        var aux=nome;
+
+        Console.WriteLine(aux);
+
+
+    }   
+}
